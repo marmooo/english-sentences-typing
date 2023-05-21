fcb4525c9eb4d641652683f5e740426e716248ce
marmooo <marmooo@users.noreply.github.com>
2023-05-22 08:33:18 +0900

    cleanup

--- src/index.js
+++ src/index.js
@@ -166,8 +166,8 @@ function toggleKeyboard() {
   }
 }
 
-function toggleGuide() {
-  if (this.checked) {
+function toggleGuide(event) {
+  if (event.target.checked) {
     guide = true;
   } else {
     guide = false;
@@ -636,13 +636,13 @@ function initTime() {
   document.getElementById("time").textContent = gameTime;
 }
 
-function changeMode() {
-  if (this.textContent == "EASY") {
-    this.textContent = "NORMAL";
-  } else if (this.textContent == "NORMAL") {
-    this.textContent = "HARD";
+function changeMode(event) {
+  if (event.target.textContent == "EASY") {
+    event.target.textContent = "NORMAL";
+  } else if (event.target.textContent == "NORMAL") {
+    event.target.textContent = "HARD";
   } else {
-    this.textContent = "EASY";
+    event.target.textContent = "EASY";
   }
 }
 
