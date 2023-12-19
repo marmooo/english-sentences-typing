fe045c0ea78b7176bc85d7bc977d4b8a3d561398
marmooo <marmooo@users.noreply.github.com>
2023-12-20 08:51:35 +0900

    add shift patch

--- src/index.js
+++ src/index.js
@@ -340,7 +340,7 @@ function removeGuide(currNode) {
     simpleKeyboard.setOptions({ layoutName: "default" });
   } else {
     const shift = simpleKeyboard.getButtonElement("{shift}");
-    shift.classList.remove("guide");
+    if (shift) shift.classList.remove("guide");
   }
 }
 
@@ -353,7 +353,7 @@ function showGuide(currNode) {
       button.classList.add("guide");
     } else {
       const shift = simpleKeyboard.getButtonElement("{shift}");
-      shift.classList.add("guide");
+      if (shift) shift.classList.add("guide");
     }
   }
 }
