40beb51add41e77b0805084900259494a11ae82c
marmooo <marmooo@users.noreply.github.com>
2025-07-09 00:15:11 +0900

    improve scroll

--- src/index.js
+++ src/index.js
@@ -642,7 +642,7 @@ function countdown() {
       scorePanel.classList.add("d-none");
       resizeFontSize(aa);
       globalThis.scrollTo({
-        top: document.getElementById("gamePanel").getBoundingClientRect().top,
+        top: document.getElementById("aaOuter").getBoundingClientRect().top,
         behavior: "auto",
       });
       typable();
